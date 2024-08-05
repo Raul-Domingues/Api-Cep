ee243b2c5dade2fbb62523ff57a3a92d493eab40
Raul Domingues <raul.domingues@db1.com.br>
2024-08-05 14:26:24 -0300

    update

--- src/app/steps/select-plan/select-plan.component.ts
+++ src/app/steps/select-plan/select-plan.component.ts
@@ -1,18 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { LocalStorageService } from '../../services/local-storage.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'select-plan',
   templateUrl: './select-plan.component.html',
   styleUrl: './select-plan.component.scss',
 })
-export class SelectPlanComponent implements OnInit{
-  selectedPlan: string | null = null;
+export class SelectPlanComponent implements OnInit {
+  selectedPlan!: string;
+  duration: string = 'monthly'
+
+  planForm!: FormGroup;
+
+  constructor(
+    private router: Router,
+    private localStorage: LocalStorageService,
+    private fb: FormBuilder
+  ) {}
+
+  ngOnInit(): void {
+    console.log('teste no ngOnInit');
+  }
+
+  toggleChange() {
+    this.duration = this.duration === 'monthly' ? 'yearly' : 'monthly';
+    console.log(this.selectedPlan);
+    console.log(this.duration);
+  }
 
   selectPlan(option: string) {
     this.selectedPlan = option;
   }
 
-  ngOnInit(): void {
-    console.log('teste')
+  nextStep() {
+    this.localStorage.setItem('form3', {
+      selectedPlan: this.selectedPlan,
+      duration: this.duration,
+    });
+    this.router.navigate(['/summary']);
   }
 }
